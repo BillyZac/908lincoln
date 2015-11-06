382f5ca0bb583390653240027c3258bd7afacadc
Zachary Smith <billyzacsmith@gmail.com>
2015-11-06 16:05:50 -0700

    Comment out all the stuff we dont need. We only want the title of the Reddit post, which we"ll pass to the canvas later.

--- js/reddit.js
+++ js/reddit.js
@@ -1,4 +1,4 @@
-var section = document.getElementById('posts')
+// var section = document.getElementById('posts')
 var global
 var request = new XMLHttpRequest
 
@@ -8,22 +8,22 @@ request.onreadystatechange = function() {
     global = response
     for (var i=1; i<20; i++) {
       // Create post div
-      var post = document.createElement('div')
-      post.className = "post"
+      // var post = document.createElement('div')
+      // post.className = "post"
 
       // Thumbnail
-      var thumbURL = response.data.children[i].data.thumbnail
-      var newPic = document.createElement('img')
-      newPic.className = 'thumb'
-      newPic.src = thumbURL
-      post.appendChild(newPic)
+      // var thumbURL = response.data.children[i].data.thumbnail
+      // var newPic = document.createElement('img')
+      // newPic.className = 'thumb'
+      // newPic.src = thumbURL
+      // post.appendChild(newPic)
 
       // Score
-      var score = global.data.children[i].data.score
-      var newScore = document.createElement('span')
-      newScore.className = 'score'
-      newScore.innerText = score
-      post.appendChild(newScore)
+      // var score = global.data.children[i].data.score
+      // var newScore = document.createElement('span')
+      // newScore.className = 'score'
+      // newScore.innerText = score
+      // post.appendChild(newScore)
 
       // Title
       var title = global.data.children[i].data.title
@@ -33,10 +33,11 @@ request.onreadystatechange = function() {
       var newTitle = document.createElement('span')
       newTitle.className = 'title'
       newTitle.innerText = title
-      post.appendChild(newTitle)
+      console.log(title)
+      // post.appendChild(newTitle)
 
       // Add post to section
-      section.appendChild(post)
+      // section.appendChild(post)
 
     }
   }
